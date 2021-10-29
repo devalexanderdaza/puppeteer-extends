5bf319e6d285049d2efb7abbe334849ba3153303
Alexander Daza <dev.alexander.daza@gmail.com>
2021-10-29 03:21:47 -0500

    feat(docs): removed docs-ts and implemented typedocs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
-/**
- * @since 1.0.0
- */
 import * as PuppeteerExtends from './main/puppeteer';
 import { Logger } from './main/logger';
 
+
 /**
- * @since 1.0.0
+ * Main instances of puppeteer-extends
+ *
+ * ```typescript
+ * import { PuppeteerExtends, Logger } from 'puppeteer-extends';
+ *
+ * const main = async () => {
+ *  const browser = await PuppeteerExtends.getBrowser({ isHeadless: true });
+ *      if (browser) {
+ *          const page = await browser.newPage();
+ *          // An example of crawling a page with CloudFlare applied.
+ *          Logger.debug('🚧  Crawling in progress...');
+ *          const url = 'https://github.com/devalexanderdaza';
+ *          await PuppeteerExtends.goto(page, url);
+ *          await page.screenshot({ path: 'devalexanderdaza-github-screenshot.png' });
+ *
+ *          Logger.debug('🚧  Crawling is complete.');
+ *          Logger.debug('🚧  Exit the Puppeteer...');
+ *          await browser.close();
+ *      }
+ * }
+ *
+ * main().then(r => {});
+ * ```
  */
 export { PuppeteerExtends, Logger };
