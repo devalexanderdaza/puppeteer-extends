3cbfb1747d1b2823dd818cb228304777c0060ca4
Alexander Daza <dev.alexander.daza@gmail.com>
2021-10-30 07:46:13 -0500

    Update release.config.js

--- release.config.js
+++ release.config.js
@@ -18,7 +18,7 @@ module.exports = {
 		[
 			'@semantic-release/git',
 			{
-				assets: ['package.json', 'package-lock.json', 'npm-shrinkwrap.json', 'CHANGELOG.md', 'README.md', 'dist/**', 'docs/**'],
+				assets: ['package.json', 'package-lock.json', 'CHANGELOG.md', 'README.md', 'dist/**', 'docs/**'],
 				message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
 			}
 		]
