1a28381ea3ddc87e8228702955a7c34259ffbd98
Alexander Daza <dev.alexander.daza@gmail.com>
2021-10-30 07:32:32 -0500

    fix(actions): test release action

--- release.config.js
+++ release.config.js
@@ -11,12 +11,14 @@ module.exports = {
 				changelogFile: 'CHANGELOG.md'
 			}
 		],
-		'@semantic-release/npm',
+		["@semantic-release/npm", {
+			"pkgRoot": "dist",
+		}],
 		'@semantic-release/github',
 		[
 			'@semantic-release/git',
 			{
-				assets: ['package.json', 'package-lock.json', 'CHANGELOG.md', 'README.md', 'dist/**', 'docs/**'],
+				assets: ['package.json', 'package-lock.json', 'npm-shrinkwrap.json', 'CHANGELOG.md', 'README.md', 'dist/**', 'docs/**'],
 				message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
 			}
 		]
