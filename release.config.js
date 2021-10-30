d9407449f6759d98b5ce6714614b6f69c7c46666
Alexander Daza <dev.alexander.daza@gmail.com>
2021-10-30 08:30:29 -0500

    feat(actions): refactor release for best performance

--- release.config.js
+++ release.config.js
@@ -11,9 +11,7 @@ module.exports = {
 				changelogFile: 'CHANGELOG.md'
 			}
 		],
-		["@semantic-release/npm", {
-			"pkgRoot": "dist",
-		}],
+		"@semantic-release/npm",
 		'@semantic-release/github',
 		[
 			'@semantic-release/git',
