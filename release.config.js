8dc5229b9cb6c163fc69d843cbf013296c904d3a
Alexander Daza <dev.alexander.daza@gmail.com>
2025-04-13 01:30:29 -0500

    fix(release): migrate to module.exports and streamline plugin configuration

--- release.config.js
+++ release.config.js
@@ -1,70 +1,24 @@
-export const extendConfig = [
-	'semantic-release-npm-github-publish',
-];
-export const branches = [
-	{ name: 'main', channel: 'latest', range: 'main' },
-];
-export const plugins = [
-	[
-		"@semantic-release/commit-analyzer",
-		{
-			"releaseRules": [
-				{
-					"type": "build",
-					"release": "patch"
-				},
-				{
-					"type": "ci",
-					"release": "patch"
-				},
-				{
-					"type": "chore",
-					"release": "patch"
-				},
-				{
-					"type": "docs",
-					"release": "patch"
-				},
-				{
-					"type": "refactor",
-					"release": "patch"
-				},
-				{
-					"type": "style",
-					"release": "patch"
-				},
-				{
-					"type": "test",
-					"release": "patch"
-				},
-				{
-					"type": "feat",
-					"release": "minor"
-				},
-				{
-					"type": "fix",
-					"release": "patch"
-				},
-				{
-					"type": "perf",
-					"release": "patch"
-				},
-				{
-					"type": "BREAKING CHANGE",
-					"release": "major"
-				}
-			]
-		}
+module.exports = {
+	branches: [
+		'main'
 	],
-	'@semantic-release/release-notes-generator',
-	'@semantic-release/changelog',
-	'@semantic-release/npm',
-	[
-		'@semantic-release/git',
-		{
-			assets: ['package.json', 'pnpm-lock.yaml', 'CHANGELOG.md', 'README.md', 'dist/**', 'docs/**'],
-			message: 'release(version): Release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-		}
-	],
-	"@semantic-release/github",
-];
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		[
+			'@semantic-release/changelog',
+			{
+				changelogFile: 'CHANGELOG.md'
+			}
+		],
+		"@semantic-release/npm",
+		'@semantic-release/github',
+		[
+			'@semantic-release/git',
+			{
+				assets: ['package.json', 'package-lock.json', 'CHANGELOG.md', 'README.md', 'dist/**', 'docs/**'],
+				message: 'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+			}
+		]
+	]
+}
